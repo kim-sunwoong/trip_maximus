2e9497d39d2b0daaf1f2be77c0e9ae1f7f28ef41
wonseok-yu <awofg46@gmail.com>
2021-06-27 13:50:16 +0900

    연결 안된거 해결

--- src/main/java/com/maximusteam/tripfulaxel/admin/controller/AdminController.java
+++ src/main/java/com/maximusteam/tripfulaxel/admin/controller/AdminController.java
@@ -1,20 +1,24 @@
 package com.maximusteam.tripfulaxel.admin.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.maximusteam.tripfulaxel.admin.model.dto.AdminDTO;
 import com.maximusteam.tripfulaxel.admin.model.service.AdminService;
 
 @Controller
 @RequestMapping("/admin/*")
 public class AdminController {
 
+//	@Qualifier("adminServiceImpl")
 	private final AdminService adminService;
-	
-	@Autowired 
+	 
+	@Autowired
 	public AdminController(AdminService adminService) {
 		this.adminService = adminService;
 	}
@@ -22,7 +26,10 @@ public class AdminController {
 	@GetMapping("workerList") // 얘는 화면에서 우리가 여기로 오라고 주소를 정해주는 것이고
 	public String selectWorkerList(Model model) {
 		
-		model.addAttribute("selectAdmin", adminService.selectAdminList());
+		System.out.println("여기는 된단 소린데..");
+		List<AdminDTO> adminList = adminService.selectAdminList();
+		model.addAttribute("selectAdmin", adminList);
+		System.out.println("여기가 안된다 소리..");
 			
 		return "admin/workerList"; // 리턴값의 주소는 받아온 값을 가지고 다시 이 화면으로 가라는 것이다.
 	}
--- src/main/java/com/maximusteam/tripfulaxel/admin/model/service/AdminServiceImpl.java
+++ src/main/java/com/maximusteam/tripfulaxel/admin/model/service/AdminServiceImpl.java
@@ -18,22 +18,25 @@ public class AdminServiceImpl implements AdminService {
 	
 	@Autowired
 	public AdminServiceImpl(AdminMapper mapper) {
+		System.out.println("연결제발!!!!!!!!!!!!!!!");
 		this.mapper = mapper;
 	}
 
 	@Override
 	public List<AdminDTO> selectAdminList() {
+		System.out.println("관리자 리스트111");
 		return mapper.selectAdmin();
 	}
 
 	@Override
 	public List<MemberDTO> selectMemberList() {
+		System.out.println("회원관리 리스트222");
 		return mapper.selectMember();
 	}
 
 	@Override
 	public List<ReportDTO> selectReportList() {
-		
+		System.out.println("신고 리스트333");
 		return mapper.selectReport();
 	}
 
